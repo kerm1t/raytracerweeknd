10f6cda0f46e2b5568400c4185e9dc7888027632
kerm1t <wolfgang.schulz@gmail.com>
2020-10-18 08:45:28 +0200

    fix const function

--- ray.h
+++ ray.h
@@ -9,5 +9,5 @@ public:
   ray(const vec3& a, const vec3& b) { A = a; B = b; }
   vec3 origin() const { return A; } // const function, i.e. not allowed ray.origin = ...
   vec3 dir() const { return B; }
-  vec3 point_at_param(float t) { return A + t * B; }
+  vec3 point_at_param(float t) const { return A + t * B; }
 };
