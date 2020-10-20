ef841eb77163c86eb76697fb7dfe521ae4feef36
kerm1t <wolfgang.schulz@gmail.com>
2020-10-20 09:01:46 +0200

    chapter8, added metal fuzziness

--- material.h
+++ material.h
@@ -40,13 +40,14 @@ vec3 reflect(const vec3& v, const vec3& n) {
 
 class metal : public material {
 public:
-  metal(const vec3& a) : albedo(a){}
+  metal(const vec3& a, float f) : albedo(a) { if (f < 1) fuzz = f; else fuzz = 1; }
   virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const
   {
     vec3 reflected = reflect(Normalize(r_in.dir()), rec.normal);
-    scattered = ray(rec.p, reflected);
+    scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
     attenuation = albedo;
     return true;
   }
   vec3 albedo;
+  float fuzz;
 };
