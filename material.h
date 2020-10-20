48befe88276c6f17af4e729dcbb26288a7f46b87
kerm1t <wolfgang.schulz@gmail.com>
2020-10-20 09:24:48 +0200

    chapt.9 - dielectrics (e.g. glass)

--- material.h
+++ material.h
@@ -44,10 +44,59 @@ public:
   virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const
   {
     vec3 reflected = reflect(Normalize(r_in.dir()), rec.normal);
-    scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
+    scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
     attenuation = albedo;
     return true;
   }
   vec3 albedo;
   float fuzz;
+};
+
+//////////////// DIELECTRIC (non electric) e.g. Glass ////////////////
+// typically air = 1, glass = 1.3 - 1.7, diamond = 2.4
+bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted)
+{
+  vec3 uv = Normalize(v);
+  float dt = Dot(uv, n);
+  float discriminant = 1.0 - ni_over_nt * ni_over_nt * (1 - dt * dt);
+  if (discriminant > 0) {
+    refracted = ni_over_nt * (uv - n * dt) - n * sqrt(discriminant);
+    return true;
+  }
+  else {
+    return false;
+  }
+};
+
+class dielectric : public material {
+public:
+  dielectric(float ri) : ref_idx(ri) {}
+  virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const
+  {
+    vec3 outward_normal;
+    vec3 reflected = reflect(r_in.dir(), rec.normal);
+    float ni_over_nt;
+    attenuation = vec3(1.0, 1.0, 1.0);
+    vec3 refracted;
+    if (Dot(r_in.dir(), rec.normal) > 0) {
+      outward_normal = -rec.normal;
+      ni_over_nt = ref_idx;
+    }
+    else
+    {
+      outward_normal = rec.normal;
+      ni_over_nt = 1.0 / ref_idx;
+    }
+    if (refract(r_in.dir(), outward_normal, ni_over_nt, refracted))
+    {
+      scattered = ray(rec.p, refracted);
+    }
+    else
+    {
+      scattered = ray(rec.p, refracted);
+      return false;
+    }
+    return true;
+  }
+  float ref_idx;
 };
