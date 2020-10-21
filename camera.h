cbb7a45d139b6a997fed7a73281f8cc7f2d2f73e
kerm1t <wolfgang.schulz@gmail.com>
2020-10-21 08:15:22 +0200

    chapt. 10a) camera

--- camera.h
+++ camera.h
@@ -1,15 +1,19 @@
 #pragma once
 
 #include "ray.h"
+#define M_PI 3.145
 
 class camera
 {
 public:
-  camera()
+  camera(float vfov, float aspect) // vfov: top to bottom [deg]
   {
-    bottom_left =  vec3(-2.0, -1.5, -1.0);
-    horiz = vec3(4.0, 0.0, 0.0);
-    vert = vec3(0.0, 3.0, 0.0);
+    float theta = vfov * M_PI / 180.0;
+    float half_height = tan(theta / 2);
+    float half_width = aspect * half_height;
+    bottom_left =  vec3(-half_width,-half_height,-1.0);
+    horiz = vec3(2*half_width, 0.0, 0.0);
+    vert = vec3(0.0, 2*half_height, 0.0);
     origin = vec3(0.0, 0.0, 0.0);
   }
   ray get_ray(float u, float v) { return ray(origin, bottom_left + u * horiz + v * vert); }
--- raytracerweeknd.cpp
+++ raytracerweeknd.cpp
@@ -40,14 +40,16 @@ int main()
 
   std::cout << "P3\n" << w << ' ' << h << "\n255\n";
  
-  hitable* list[5];
-  list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1,0.2,0.5)));
-  list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0))); // that is the green sphere ;-)
-  list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.2));
-  list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
-  list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
-  hitable* world = new hitable_list(list, 5);
-  camera cam;
+  float R = cos(M_PI / 180.0);
+  hitable* list[3];
+  list[0] = new sphere(vec3(-R, 0, -1), R, new lambertian(vec3(0,0,1)));
+  list[1] = new sphere(vec3(R, 0, -1), R, new lambertian(vec3(1, 0, 0)));
+  list[2] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0))); // that is the green sphere ;-)
+//  list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.2));
+//  list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
+ // list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
+  hitable* world = new hitable_list(list, 3);
+  camera cam(90,float(w)/float(h));
 
   for (int j = h - 1; j >= 0; j--)
   {
