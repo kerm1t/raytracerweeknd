770bae3bf360755a6bbf8245efdac5377e22266b
kerm1t <wolfgang.schulz@gmail.com>
2020-10-23 10:28:46 +0200

    chapt. 12 - random scene

--- raytracerweeknd.cpp
+++ raytracerweeknd.cpp
@@ -31,16 +31,45 @@ vec3 color(const ray& r, hitable* world, int depth)
   }
 }
 
+hitable* random_scene() {
+  int n = 500;
+  hitable** list = new hitable * [n + 1];
+  list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(.5, .5, .5)));
+  int i = 1;
+  for (int a = -11; a < 11; a++) {
+    for (int b = -11; b < 11; b++) {
+      float choose_mat = rand_f();
+      vec3 center(a + 0.9 * rand_f(), 0.2, b + 0.9 * rand_f());
+      if (Length(center - vec3(4, 0.2, 0)) > 0.9) {
+        if (choose_mat < 0.8) { // diffuse
+          list[i++] = new sphere(center, 0.2, new lambertian(vec3(rand_f() * rand_f(), rand_f() * rand_f(), rand_f() * rand_f())));
+        }
+        else if (choose_mat < 0.95) { // metal
+          list[i++] = new sphere(center, 0.2, new metal(vec3(0.5 * (1 + rand_f()), 0.5 * (1 + rand_f()), 0.5 * rand_f()),0.2));
+        }
+        else { // glass
+          list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+        }
+      }
+    }
+  }
+  list[i++] = new sphere(vec3(0,1,0), 1.0, new dielectric(1.5));
+  list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4,0.2,0.1)));
+  list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7,0.6,0.5), 0.2));
+
+  return new hitable_list(list, i);
+}
+
 int main()
 {
-  const int w = 640;
-  const int h = 480;
+  const int w = 1024;
+  const int h = 768;
   const int ns = 100;
  
   std::cout << "P3\n" << w << ' ' << h << "\n255\n";
  
   // objects
-  float R = cos(M_PI / 180.0);
+/*  float R = cos(M_PI / 180.0);
   hitable* list[6];
   list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
   list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0))); // that is the green sphere ;-)
@@ -49,13 +78,16 @@ int main()
   list[4] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
   list[5] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
   hitable* world = new hitable_list(list, 6);
-  
+*/  hitable* world = random_scene();
+
   // camera
-  vec3 pos(3, 3, 2);
+//  vec3 pos(7, 1.6, -1);
+//  vec3 lookat(-6, 1.3, 1);
+  vec3 pos(5, 1.5, 2);
   vec3 lookat(0, 0, -1);
   float dist_to_focus = Length(pos - lookat);
-  float aperture = 2.0;
-  camera cam(pos, lookat, vec3(0,1,0), 30, float(w)/float(h), aperture, dist_to_focus);
+  float aperture = 0.01;
+  camera cam(pos, lookat, vec3(0,1,0), 90, float(w)/float(h), aperture, dist_to_focus);
 
   // Render
   for (int j = h - 1; j >= 0; --j)
